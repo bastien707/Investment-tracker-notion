4a2d222ef2133077e3b49fa314ea98efe690c26a
bastien707 <bastien.facqueur@efrei.net>
2022-07-25 00:29:21 +0200

    [UPDATE] add current amount dynamic display

--- src/updateDatabase.js
+++ src/updateDatabase.js
@@ -1,15 +1,30 @@
 import {Client} from "@notionhq/client";
-import fetch from "node-fetch";
-import dotenv from 'dotenv';
-dotenv.config();
+import getPriceBTC from "./getPriceBTC.js";
+import * as config from '../utility/index.js';
 
-const notion = new Client({ auth: process.env.NOTION_API_KEY });
+const notion = new Client({ auth: config.env.notionKey });
 
-const getDB = async () => {
-    const databaseId = process.env.FINANCING_TABLE;
+const getDatabase = async () => {
+    const databaseId = config.env.financialDB;
     const response = await notion.databases.retrieve({ database_id: databaseId });
-    console.log(response.properties.Value);
+    console.log(response);
 };
+
+const updateCurrentAmount = async () => {
+    const databaseId = config.env.financialDB;
+    return await notion.databases.update({
+        database_id: databaseId,
+        properties: {
+            ["Current amount value"]: {
+                formula: {
+                    expression: `prop('Amount') * ${await getPriceBTC()}`
+                }
+            }
+        }
+    });
+};
+
+setInterval(updateCurrentAmount, 3000);
 
 
 
