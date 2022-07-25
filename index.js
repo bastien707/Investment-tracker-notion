ca1e52c31efdf57f62c796d8025e1f9796dd15ae
bastien707 <bastien.facqueur@efrei.net>
2022-07-25 11:46:21 +0200

    [RENAME] rename files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Client } from "@notionhq/client";
 import * as config from "./utility/index.js";
-import { updateCurrentAmount } from "./src/updateDatabase.js";
-import { updateBTC } from "./src/updateBTC.js";
+import { updateCurrentAmount } from "./src/database.js";
+import { updateBTC } from "./src/blocks.js";
 const notion = new Client({ auth: config.env.notionKey });
 
 setInterval(() => {
