0476d9fc4a2ed64640bd1645a879959120bdc296
bastien707 <bastien.facqueur@efrei.net>
2022-08-08 23:26:51 +0200

    [UPDATE] adding elrond currency

--- src/blocks.js
+++ src/blocks.js
@@ -1,4 +1,4 @@
-import {getPriceBTC, getPriceETH} from "./getPrices.js";
+import {getPriceBTC, getPriceEGLD, getPriceETH} from "./getPrices.js";
 
 export const getBlock = async (notion, blockId) => {
     const response = await notion.blocks.retrieve({ block_id: blockId });
@@ -99,6 +99,64 @@ export const updateEthereumBlock = async (notion, block) => {
                         {
                             "text": {
                                 content: `$ ${ETH.lastPrice.slice(0,9)} (${ETH.priceChangePercent}%) `
+                            },
+                            annotations: {
+                                bold: true,
+                                color: 'red'
+                            },
+                        },
+                    ],
+                    icon: {
+                        external: {
+                            url: "https://cdn-icons-png.flaticon.com/512/7090/7090863.png"
+                        },
+                    },
+                    color: "red_background"
+                }
+            });
+        }
+
+    } catch (error) {
+        return error;
+    }
+}
+
+export const updateEGoldBlock = async (notion, block) => {
+    const blockId = block;
+    let EGLD =  await getPriceEGLD();
+
+    try {
+        if(EGLD.priceChangePercent >= 0){
+            await notion.blocks.update({
+                block_id: blockId,
+                "callout": {
+                    "rich_text": [
+                        {
+                            "text": {
+                                content: `$ ${EGLD.lastPrice.slice(0,9)} (${EGLD.priceChangePercent}%) `
+                            },
+                            annotations: {
+                                bold: true,
+                                color: 'green'
+                            },
+                        },
+                    ],
+                    icon: {
+                        external: {
+                            url: "https://cdn-icons-png.flaticon.com/512/7090/7090972.png"
+                        },
+                    },
+                    color: "green_background"
+                }
+            });
+        } else {
+            await notion.blocks.update({
+                block_id: blockId,
+                "callout": {
+                    "rich_text": [
+                        {
+                            "text": {
+                                content: `$ ${EGLD.lastPrice.slice(0,9)} (${EGLD.priceChangePercent}%) `
                             },
                             annotations: {
                                 bold: true,
