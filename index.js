2c01a0155accc5a5341c3f59ac6d5729f597012c
bastien707 <bastien.facqueur@efrei.net>
2023-04-02 11:35:16 +0200

    chore: refactor

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 import { Client } from "@notionhq/client";
 import * as config from "./utility/index.js";
-import {updateBitcoinToDatabase, updateEGoldToDatabase, updateEthereumToDatabase} from "./src/database.js";
-import {updateBitcoinBlock, updateEGoldBlock, updateEthereumBlock} from "./src/blocks.js";
+import {
+  updateBitcoinToDatabase,
+  updateEGoldToDatabase,
+  updateEthereumToDatabase,
+} from "./src/database.js";
+import {
+  updateBitcoinBlock,
+  updateEGoldBlock,
+  updateEthereumBlock,
+} from "./src/blocks.js";
 
 const notion = new Client({ auth: config.env.notionKey });
 
-const run = () => {
-    try {
-        setInterval(() => {
-            updateBitcoinBlock(notion, config.env.btcBlock).then();
-            updateBitcoinToDatabase(notion, config.env.btcTable).then();
-            updateEthereumBlock(notion, config.env.ethBlock).then();
-            updateEthereumToDatabase(notion, config.env.ethTable).then();
-            updateEGoldBlock(notion, config.env.egldBlock).then();
-            updateEGoldToDatabase(notion, config.env.egldTable).then();
-        }, 5000);
-    } catch (error) {
-        console.log("Error in main: " + error);
-    }
-}
+const updateBlocks = async () => {
+  try {
+    Promise.all([
+      updateBitcoinBlock(notion, config.env.btcBlock),
+      updateEthereumBlock(notion, config.env.ethBlock),
+      updateEGoldBlock(notion, config.env.egldBlock),
+    ]);
+  } catch (error) {
+    console.log("Error in updateBlocks: " + error);
+  }
+};
+
+const updateDatabase = async () => {
+  try {
+    Promise.all([
+      updateBitcoinToDatabase(notion, config.env.btcTable),
+      updateEthereumToDatabase(notion, config.env.ethTable),
+      updateEGoldToDatabase(notion, config.env.egldTable),
+    ]);
+  } catch (error) {
+    console.log("Error in updateDatabase: " + error);
+  }
+};
+
+const run = async () => {
+  try {
+    setInterval(async () => {
+      await updateBlocks();
+      await updateDatabase();
+    }, 10000);
+  } catch (error) {
+    console.log("Error in main: " + error);
+  }
+};
 
 run();
-
-
